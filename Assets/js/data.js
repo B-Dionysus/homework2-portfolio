07665400de65259888e0eb2cd4489e3cca64d5dd
Benjamin Dionysus <benjamin@sixbynine.com>
2020-10-10 12:17:13 -0500

    Moved data to seperate file

---
+++ Assets/js/data.js
@@ -0,0 +1,61 @@
+function getData(){
+    var assignmentArray=[
+        {
+            "name":"Project 1",
+            "url":"https://b-dionysus.github.io/project1-Election/",
+            "git":"https://github.com/B-Dionysus/project1-Election",
+            "thumb":"project1.PNG",
+            "path":"./Assets/Images/"
+        },
+        {
+            "name":"Prework",
+            "url":"https://b-dionysus.github.io/prework-about-me/",
+            "git":"https://github.com/B-Dionysus/prework-about-me",
+            "thumb":"prework.PNG",
+            "path":"./Assets/Images/"
+        },
+        {
+            "name":"Portfolio",
+            "url":"https://b-dionysus.github.io/homework2-portfolio/",
+            "git":"https://github.com/B-Dionysus/homework2-portfolio",
+            "thumb":"hw2.PNG",
+            "path":"./Assets/Images/"
+        },
+        {
+            "name":"Password Generator",
+            "url":"https://b-dionysus.github.io/homework2-passwordGenerator/",
+            "git":"https://github.com/B-Dionysus/homework2-passwordGenerator",
+            "thumb":"hw3.PNG",
+            "path":"./Assets/Images/"
+        },
+        {
+            "name":"Quiz",
+            "url":"https://b-dionysus.github.io/homework4-quiz/",
+            "git":"https://github.com/B-Dionysus/homework4-quiz",
+            "thumb":"hw4.PNG",
+            "path":"./Assets/Images/"
+        },
+        {
+            "name":"Day Planner",
+            "url":"https://b-dionysus.github.io/homework05-Calendar/Develop/",
+            "git":"https://github.com/B-Dionysus/homework05-Calendar/",
+            "thumb":"hw5.PNG",
+            "path":"./Assets/Images/"
+        },
+        {
+            "name":"The Weather",
+            "url":"https://b-dionysus.github.io/homework6-Weather/",
+            "git":"https://github.com/B-Dionysus/homework6-Weather",
+            "thumb":"hw6.PNG",
+            "path":"./Assets/Images/"
+        },
+        {
+            "name":"The Weather",
+            "url":"https://b-dionysus.github.io/homework6-Weather/",
+            "git":"https://github.com/B-Dionysus/homework6-Weather",
+            "thumb":"hw6.PNG",
+            "path":"./Assets/Images/"
+        },
+    ];
+    return assignmentArray;   
+}
